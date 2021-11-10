29688b305a04d2602d0e9a8b0d081c69e20b9547
Oleg <OlegMail>
2021-11-11 02:43:43 +0300

    121 add Spring beans

--- register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -1,4 +1,28 @@
 package edu.javacourse.register.dao;
 
+import edu.javacourse.register.domain.MarriageCertificate;
+import edu.javacourse.register.view.MarriageRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
+
+
 public class MarriageDao {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
+
+    private EntityManager entityManager;
+    private String test;
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    //TODO доделать по аналогии с персон дао только под брачные сертификаты
+    public MarriageCertificate findMarriageCertificate(MarriageRequest request) {
+        LOGGER.info("findMarriageCertificate called:{}", test);
+
+        return null;
+    }
 }
