aa4396b3c92513bd7c83cecc177e32484f9fd571
Oleg <OlegMail>
2021-11-16 01:13:21 +0300

    122 + @annotations

--- register-office/src/main/java/edu/javacourse/register/Starter.java
+++ register-office/src/main/java/edu/javacourse/register/Starter.java
@@ -9,6 +9,7 @@ public class Starter {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext (new String[]{"springContext.xml"});
 
+//        MarriageController controller = context.getBean(MarriageController.class);
         MarriageController controller = context.getBean("controller", MarriageController.class);
         controller.findMarriageCertificate(new MarriageRequest());
     }
