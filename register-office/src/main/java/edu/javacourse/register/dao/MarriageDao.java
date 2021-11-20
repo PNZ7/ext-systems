aaebb8bdc7e9888534c444cc480c3736378c4121
Oleg <OlegMail>
2021-11-20 19:09:41 +0300

    125 Spring DATA JPA  @Repository

--- register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -1,31 +1,12 @@
 package edu.javacourse.register.dao;
 
 import edu.javacourse.register.domain.MarriageCertificate;
-import edu.javacourse.register.view.MarriageRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
 
-@Component
-public class MarriageDao {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
+@Repository
+public interface MarriageDao extends JpaRepository<MarriageCertificate,Long> {
 
-    private EntityManager entityManager;
-    @Value("${test.value}")
-    private String test;
-
-    public void setTest(String test) {
-        this.test = test;
-    }
-
-    //TODO доделать по аналогии с персон дао только под брачные сертификаты
-    public MarriageCertificate findMarriageCertificate(MarriageRequest request) {
-        LOGGER.info("findMarriageCertificate called:{}", test);
-
-        return null;
-    }
 }
--- register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -12,11 +12,9 @@ public class PersonDao {
 
     public List<Person> findPersons() {
         Query query = entityManager.createNamedQuery("Person.findPersons");
-        query.setParameter("personId", 1L);
         return query.getResultList();
 
     }
-
 
     public Long addPerson(Person person) {
 
