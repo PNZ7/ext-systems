3f7d3a88345bc96f2ba31f0d0769053444a64aca
Oleg <OlegMail>
2021-12-06 21:48:16 +0300

    137 integrate the servlet and Spring Framework

---
+++ student/src/main/java/edu/javacourse/student/dao/UniversityRepository.java
@@ -0,0 +1,8 @@
+package edu.javacourse.student.dao;
+
+import edu.javacourse.student.domain.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversityRepository extends JpaRepository<University, Long> {
+
+}
