189be6d0ff9c9bdeb5d9c455dae3dc280168911c
Oleg <OlegMail>
2021-12-09 02:57:34 +0300

    140   3 ways to solve LazyInitializationException       -1 FetchType.EAGER       -2 Hibernate.initialize (u.getFaculties ())       -3 in public interface UniversityRepository      @Query (name = "Universuty.findAllWithFaculties",                    value = "SELECT u FROM University u LEFT JOIN FETCH u.faculties")       It is LEFT JOIN FETCH that makes the forced unload

--- student/src/main/java/edu/javacourse/student/dao/UniversityRepository.java
+++ student/src/main/java/edu/javacourse/student/dao/UniversityRepository.java
@@ -2,7 +2,14 @@ package edu.javacourse.student.dao;
 
 import edu.javacourse.student.domain.University;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UniversityRepository extends JpaRepository<University, Long> {
+
+    @Query(name = "Universuty.findAllWithFaculties",
+            value = "SELECT u FROM University u LEFT JOIN FETCH u.faculties ")
+    List<University> findFullList();
 
 }
